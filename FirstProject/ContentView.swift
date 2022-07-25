c1bfe432a435ca8d71c3000890964f952ce70303
Carlos Evelo <carlosevelo96@gmail.com>
2022-07-25 12:36:16 -0600

    Initial Commit

---
+++ FirstProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FirstProject
+//
+//  Created by Carlos Evelo on 7/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Carlos Evelo on 7/23/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
