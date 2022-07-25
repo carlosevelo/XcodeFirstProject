1e1bee174a8b53f40cbfd99edc4a118a88e48db8
Carlos Evelo <carlosevelo96@gmail.com>
2022-07-25 12:48:21 -0600

    new text

--- FirstProject/ContentView.swift
+++ FirstProject/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Hello, world!")
+            Text("Hello, world! Xcode branch")
                 .padding()
                 .font(.title)
             Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
